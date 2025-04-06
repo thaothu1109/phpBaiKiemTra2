f02d969faf4bedfcd4c5fd73cc09d8a3ad70d3c3
Mai Thu Thao <thaothumai04@gmail.com>
2025-04-06 12:15:15 +0700

    Add files via upload

---
+++ xlsignup.php
@@ -0,0 +1,7 @@
+<?php
+//Lấy thông tin và mã hóa mật khẩu 
+//Kiểm tra xem người dùng đã tồn tại hay chưa
+//Nếu tồn tại thì thông báo lỗi
+//Nếu không tồn tại thì thêm người dùng vào cơ sở dữ liệu
+//Tạo mã QR với Google Authenticator
+?>
